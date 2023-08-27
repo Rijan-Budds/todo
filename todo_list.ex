8f4eaa1a6da4c25fc5377d4ae9765a758b263a8e
Rijan-Budds <buddsrijan@gmail.com>
2023-08-27 18:21:11 +0545

    updated todolist

--- todo_list.ex
+++ todo_list.ex
@@ -1,38 +1,18 @@
 defmodule TodoList do
-  defstruct entries: []
+  defstruct tasks: []
 
-  def add_entry(todo_list, date, task) do
-    entry = %{date: date, task: task}
-    %{todo_list | entries: [entry | todo_list.entries]}
+  def add_task(todo_list, task) do
+    %TodoList{todo_list | tasks: [task | todo_list.tasks]}
   end
 
-  def edit_entry(todo_list, index, new_date, new_task) when index >= 0 and index < length(todo_list.entries) do
-    updated_entries =
-      todo_list.entries
-      |> Enum.with_index()
-      |> Enum.map(fn
-        {entry, i} when i == index ->
-          %{entry | date: new_date, task: new_task}
-        {entry, _} ->
-          entry
-      end)
-
-    %{todo_list | entries: updated_entries}
+  def complete_task(todo_list, task) do
+    tasks = Enum.filter(todo_list.tasks, fn t -> t != task end)
+    %TodoList{todo_list | tasks: tasks}
   end
 
-  def delete_entry(todo_list, index) when index >= 0 and index < length(todo_list.entries) do
-    updated_entries =
-      todo_list.entries
-      |> Enum.with_index()
-      |> Enum.reject(fn {_, i} -> i == index end)
-      |> Enum.map(fn {entry, _} -> entry end)
-
-    %{todo_list | entries: updated_entries}
-  end
-
-  def print_entries(todo_list) do
-    Enum.each(todo_list.entries, fn %{date: date, task: task} ->
-      IO.puts("#{date}: #{task}")
+  def list_tasks(todo_list) do
+    Enum.each(todo_list.tasks, fn task ->
+      IO.puts("#{task}")
     end)
   end
 end
